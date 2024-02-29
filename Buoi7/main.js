7895c002838a170358b1695c3a2da6313d40bece
dothang13 <endiezzhang13@gmail.com>
2024-02-29 23:54:00 +0700

    new

--- Buoi7/main.js
+++ Buoi7/main.js
@@ -1,11 +1,12 @@
-//Bài 1: nối mảng
-    console.log("Test1_1:");
+
+// //Bài 1: nối mảng
+//     console.log("Test1_1:");
     const a = ['a', 'b', 'c'];
     const b = [1, 2 ,3];
     const c = b.concat(a);
     console.log(c);
 
-//Bài 2: kiểm tra tất cả số chẵn trong mảng
+// //Bài 2: kiểm tra tất cả số chẵn trong mảng
     console.log("Test2_1:");
     let data = [2, 4, 6, 8]
     if (data.every((value) => value % 2 === 0)) {
@@ -23,9 +24,9 @@
     else{
         console.log("NO");
     }
-    //NO
+//     //NO
 
-//Bài 3: kiểm tra các số chỉ có duy nhất
+// //Bài 3: kiểm tra các số chỉ có duy nhất
     console.log("Test3_1:");
     let test3_1 = [1, 2, 3, 4, 5]
     if (test3_1.some((value) => value % 2 === 0)) {
@@ -43,14 +44,14 @@
     else{
         console.log("NO");
     }
-    //NO
+//     //NO
 
-//Bài 4: cho ra mảng mới chỉ có số dương
+// //Bài 4: cho ra mảng mới chỉ có số dương
     console.log("Test4_1:");
     let test4_1 = [1, -2, 3, 4, -5]
     console.log(test4_1.filter((value) => value > 0));
 
-//Bài 5: tìm số dương đầu tiên, hiển thị ra index và số đó
+// //Bài 5: tìm số dương đầu tiên, hiển thị ra index và số đó
     console.log("Test5_1:");
     let test5_1 = [-1, -2, 3, 4, -5]
     if (test5_1.some((value) => value > 0)){
@@ -80,9 +81,9 @@
     else{
         console.log("NO RESULT");
     }
-    //NO RESULT
+//     //NO RESULT
 
-//Bài 6: tìm số chia hết cho 5 bằng forEach()
+// //Bài 6: tìm số chia hết cho 5 bằng forEach()
     console.log("Test6_1");
     let test6_1 = [1, 5, 30, 26];
     test6_1.forEach(test6_1 => {
@@ -90,9 +91,9 @@
             console.log(test6_1);
         }
     });
-    //5 30
+//     //5 30
 
-//Bài 7: tìm index của số 2 đầu tiên và cuối cùng trong mảng
+// //Bài 7: tìm index của số 2 đầu tiên và cuối cùng trong mảng
     console.log("Test7_1");
     let test7_1 = [-1, 2, 3, 4, 2, 6];
     if (test7_1.some((value) => value === 2)) {
@@ -102,7 +103,7 @@
     else{
         console.log("NO RESULT");
     }
-    // 1 4
+//     // 1 4
     console.log("Test7_2");
     let test7_2 = [1, 6];
     if (test7_2.some((value) => value === 2)) {
@@ -112,9 +113,9 @@
     else{
         console.log("NO RESULT");
     }
-    // NO RESULT
+//     // NO RESULT
 
-//Bai 8: nhập các phần tử của mảng thàng 1 chuỗi
+// //Bai 8: nhập các phần tử của mảng thành 1 chuỗi
     console.log("Test8_1");
     let test8_1 = ["A", "B", "C"];
     console.log(test8_1.join(', '));
@@ -125,28 +126,29 @@
     console.log(test8_2.join(', '));
     //1, 2, 3
 
-//Bài 9: in ra mảng trị tuyệt đối của mảng đã cho
+// //Bài 9: in ra mảng trị tuyệt đối của mảng đã cho
     console.log("Test9_1");
     let test9_1 = [2, -1, -8];
     console.log(test9_1.map((value) => Math.sqrt(value*value)));
     // 2 1 8
 
-//Bài 10: Leetcode
+// //Bài 10: Leetcode
     console.log("Leetcode");
-    let leetcode = "321"
+    let leetcode = 321
     let newnum = 0;
-    while (leetcode !== 0){
+    while (leetcode != 0){
         newnum = (newnum * 10) + (leetcode % 10);
         leetcode = Math.floor(leetcode / 10)
     }
     console.log(newnum);
     // 123
 
-    let leetcode1 = "-321"
+    let leetcode1 = -321
+    leetcode1 = Math.abs(leetcode1)
     let newnum1 = 0;
-    while (leetcode1 !== 0){
+    while (leetcode1 != 0){
         newnum1 = (newnum1 * 10) + (leetcode1 % 10);
         leetcode1 = Math.floor(leetcode1 / 10)
     }
-    console.log(newnum1);
-    //-123
+    console.log("-" + newnum1);
+//     //-123
